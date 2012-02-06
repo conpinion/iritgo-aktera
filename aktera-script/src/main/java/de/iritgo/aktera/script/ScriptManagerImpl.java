cca19fee1cfca83ce4df37348040c2d9217fc3f4
grapelabs@gmail.com <grapelabs@gmail.com@71e8ea69-9a4e-8955-45a5-bfab6559e1dc>
2012-02-06 14:37:48 +0000

    git-svn-id: https://iritgo-aktera.googlecode.com/svn/trunk@94 71e8ea69-9a4e-8955-45a5-bfab6559e1dc

--- aktera-script/src/main/java/de/iritgo/aktera/script/ScriptManagerImpl.java
+++ aktera-script/src/main/java/de/iritgo/aktera/script/ScriptManagerImpl.java
@@ -20,16 +20,8 @@
 package de.iritgo.aktera.script;
 
 
+import java.util.*;
 import de.iritgo.simplelife.data.KeyedValue2;
-import org.apache.commons.beanutils.MethodUtils;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 
 /**
