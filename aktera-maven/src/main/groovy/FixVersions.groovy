b473ce26e8a2375a11bdfbed6d7e936575a5c387
grapelabs@gmail.com <grapelabs@gmail.com@71e8ea69-9a4e-8955-45a5-bfab6559e1dc>
2012-03-05 14:53:23 +0000

    git-svn-id: https://iritgo-aktera.googlecode.com/svn/trunk@99 71e8ea69-9a4e-8955-45a5-bfab6559e1dc

--- aktera-maven/src/main/groovy/FixVersions.groovy
+++ aktera-maven/src/main/groovy/FixVersions.groovy
@@ -21,107 +21,91 @@ import groovy.xml.*
  * @goal fixversions
  * @aggregator
  */
-public class FixVersionsMojo extends GroovyMojo
-{
-    void execute()
-	{
-        def projectPath = new File (".").canonicalPath
+public class FixVersionsMojo extends GroovyMojo {
+	void execute() {
+		def projectPath = new File(".").canonicalPath
 
-        def modules = [:]
+		def modules = [:]
 
-        println ("\nScanning modules...\n")
+		println("\nScanning modules...\n")
 
-        collectModules (modules, new File (projectPath, "../de.iritgo.aktera"))
-        collectModules (modules, new File (projectPath))
+		collectModules(modules, new File(projectPath, "../iritgo-aktera"))
+		collectModules(modules, new File(projectPath))
 
-        println ("\nFixing versions...\n")
+		println("\nFixing versions...\n")
 
-        updateVersions (modules)
+		updateVersions(modules)
 
-        println ()
+		println()
 	}
 
-    def collectModules (modules, path)
-    {
-        if(path.isDirectory ())
-        {
-            path.eachFileRecurse
-            {
-            	if (it.isFile () && it.getName () == 'pom.xml')
-            	{
-            		readPom (modules, it)
-            	}
-            }
-        }
-    }
+	def collectModules(modules, path) {
+		if(path.isDirectory()) {
+			path.eachFileRecurse {
+				if (it.isFile() && it.getName() == 'pom.xml') {
+					readPom(modules, it)
+				}
+			}
+		}
+	}
 
-    def readPom (modules, file)
-    {
+	def readPom(modules, file) {
 		def ns = new groovy.xml.Namespace("http://maven.apache.org/POM/4.0.0", 'ns')
-        def pom = new XmlParser ().parse (file)
-        def module = [:]
-        module.groupId = pom[ns.groupId].text ()
-        module.artifactId = pom[ns.artifactId].text ()
-        module.version = pom[ns.version].text ()
-        module.pomFile = file
-        println ("${module.groupId}.${module.artifactId} ${module.version}")
-        modules[module.groupId + '.' + module.artifactId] = module
-    }
+		def pom = new XmlParser().parse(file)
+		def module = [:]
+		module.groupId = pom[ns.groupId].text()
+		if (module.groupId == "") {
+			module.groupId = pom[ns.parent].groupId.text()
+		}
+		module.artifactId = pom[ns.artifactId].text()
+		module.version = pom[ns.version].text()
+		module.pomFile = file
+		println("${module.groupId}.${module.artifactId} ${module.version}")
+		modules[module.groupId + '.' + module.artifactId] = module
+	}
 
-    def updateVersions (modules)
-    {
+	def updateVersions(modules) {
 		def ns = new groovy.xml.Namespace("http://maven.apache.org/POM/4.0.0", 'ns')
-        modules.each
-        {
-            id, module ->
-            println (id)
-            def pom = new XmlParser ().parse (module.pomFile)
-            def modified = false
-            for (dependency in pom[ns.dependencies][ns.dependency])
-            {
-            	def depId = dependency[ns.groupId].text () + "." + dependency[ns.artifactId].text ()
-            	print ("\t${depId}")
-            	if (! modules.containsKey (depId))
-            	{
-            		println (" Skipped")
-            		continue
-            	}
-            	else if (modules[depId].version == dependency[ns.version].text ())
-            	{
-            		println (" ${dependency[ns.version].text ()} Ok")
-            		continue
-            	}
-            	print (" ${dependency[ns.version].text ()} WRONG")
-            	dependency[ns.version][0].value = modules[depId].version
-            	println (" Fixed (${dependency[ns.version].text ()})")
-            	modified = true
-            }
-            if (modified)
-            {
-            	def newPom = new File (module.pomFile.canonicalPath + ".new")
-            	newPom.withPrintWriter ()
-            	{
-            		writer ->
+		modules.each { id, module ->
+			println(id)
+			def pom = new XmlParser().parse(module.pomFile)
+			def modified = false
+			for (dependency in pom[ns.dependencies][ns.dependency]) {
+				def depId = dependency[ns.groupId].text() + "." + dependency[ns.artifactId].text()
+				print ("\t${depId}")
+				if (! modules.containsKey(depId)) {
+					println(" Skipped")
+					continue
+				}
+				else if (modules[depId].version == dependency[ns.version].text()) {
+					println (" ${dependency[ns.version].text()} Ok")
+					continue
+				}
+				print (" ${dependency[ns.version].text()} WRONG")
+				dependency[ns.version][0].value = modules[depId].version
+				println(" Fixed (${dependency[ns.version].text()})")
+				modified = true
+			}
+			if (modified) {
+				def newPom = new File(module.pomFile.canonicalPath + ".new")
+				newPom.withPrintWriter() { writer ->
 
-            		def inHeader = true
-            		module.pomFile.eachLine
-            		{
-            			if (it.startsWith ("<project"))
-            			{
-            				inHeader = false
-            			}
-            			if (inHeader)
-            			{
-            				writer.println (it)
-            			}
-            		}
+					def inHeader = true
+					module.pomFile.eachLine {
+						if (it.startsWith("<project")) {
+							inHeader = false
+						}
+						if (inHeader) {
+							writer.println(it)
+						}
+					}
 
 					def xmlNodePrinter = new XmlNodePrinter(new PrintWriter (writer), "\t")
 					xmlNodePrinter.preserveWhitespace = true
-					xmlNodePrinter.print (pom)
-            	}
-            	newPom.renameTo (module.pomFile)
-            }
-        }
-    }
+					xmlNodePrinter.print(pom)
+				}
+				newPom.renameTo(module.pomFile)
+			}
+		}
+	}
 }
